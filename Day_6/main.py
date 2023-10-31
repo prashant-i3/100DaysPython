12f15c2a6772fcc3f81aeda87dcdaa99f1975212
Prashant Pandey <prashant.pan2205@gmail.com>
2023-10-31 23:50:21 +0530

    Day_6

---
+++ Day_6/main.py
@@ -0,0 +1,26 @@
+# Functions:    
+# First define the function
+# Then Call the function
+
+
+
+def my_function():
+    print("Hello function")
+
+my_function()
+
+
+# Indentation : provides readers with a sense of continuity.
+# in Python:- Indentation in Python refers to the whitespaces (spaces or tabs) at the start of the line to indicate a block of code. The leading whitespaces (space and tabs) at the start of a line are used to determine the indentation level of the line.
+
+# For Loop:
+# for item in list_of_items:
+#     # Do something to each item
+
+# for number in range(a, b):
+#     print(number)
+
+# While 
+# while something is true:
+#     # Do something repeatedly
+
