a143e52924f867095d7d372f803c81481c125c5c
Prashant Pandey <prashant.pan2205@gmail.com>
2023-11-06 00:05:43 +0530

    Day_3

--- Day_3/Project/Project.py
+++ Day_3/Project/Project.py
@@ -27,4 +27,4 @@ print("Your mission is to find the treasure")
 choice1 = input('You\'re at a crossroad, where do you want to go? Type "left" or "right".').lower()
 
 if choice1 == "left":
-    choice2 ==    
+    choice2 == 
